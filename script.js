d048a4cd126295d3c857e83fdde98da3be3303b2
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-27 20:11:37 -0600

    refactor functions and add functionality to ui

--- script.js
+++ script.js
@@ -30,40 +30,38 @@ let numOne;
 let numTwo;
 let operation;
 let res;
-let addproduct;
 
 
-function myAdd(numOne, numTwo) {
+function myAdd() {
     return numOne + numTwo;
 }
 
-function mySubtract(numOne, numTwo) {
-    res = numOne - numTwo;
+function mySubtract() {
+    return numOne - numTwo;
 }
 
-function myMultiply(numOne, numTwo) {
-    res = numOne * numTwo;
+function myMultiply() {
+    return numOne * numTwo;
 }
 
-function myDivide(numOne, numTwo) {
-    res = numOne / numTwo;
+function myDivide() {
+    return numOne / numTwo;
 }
 
 
 
-function operate(operation) {
+function operate() {
     if (operation === '+') {
         res = myAdd();
-        console.log(alert('hi')); // 
     }
     if (operation === '-') {
-        res = mySubtract(numOne, numTwo)
+        res = mySubtract()
     }
     if (operation === '×') {
-        res = myMultiply(numOne, numTwo)
+        res = myMultiply()
     }
     if (operation === '÷') {
-        res = myDivide(numOne, numTwo)
+        res = myDivide()
     }
     console.log(res);
 }
@@ -72,17 +70,29 @@ numButtons.forEach((button) => {
     button.addEventListener('click', () => {
         if (display.textContent.length < 12) {
         display.textContent += button.textContent;
+        console.log(button.textContent); //
         }
     })
 })
 
+// bug below: if there is an operation defined, does not reset display
+// maybe solution is: if operation exists && something else, make display to numOne,
+// as the previous result would be stored in num
+
 opButtons.forEach((button) => {
     button.addEventListener('click', () => {
+        if (operation) {
+            numTwo = Number(display.textContent);
+            operate();
+            display.textContent = (Math.round(res * 100000) / 100000);
+            numOne = (Math.round(res * 100) / 100);
+        }
         if (!numOne) {
             numOne = Number(display.textContent);
         } else if (numOne) {
             numTwo = Number(display.textContent);
         }
+        if (!operation) {display.textContent = '';}
         operation = button.textContent;
         console.log(operation); //
     })
@@ -91,10 +101,27 @@ opButtons.forEach((button) => {
 
 clearButton.addEventListener('click', () => {
     display.textContent = '';
+    numOne = undefined;
+    numTwo = undefined;
+    operation = undefined;
+    res = undefined;
 })
 
 equalsButton.addEventListener('click', () => {
     numTwo = Number(display.textContent);
-    console.log(equalsButton.textContent);
+    console.log(equalsButton.textContent); // 
     operate();
+    display.textContent = (Math.round(res * 10000) / 10000);
+    numOne = (Math.round(res * 100) / 100);
+    numTwo = undefined;
+    res = undefined;
+    operation = undefined;
 });
+
+decimalButton.addEventListener('click', () => {
+    if (!display.textContent.includes(decimalButton.textContent)) {
+        display.textContent += decimalButton.textContent;
+    }
+})
+
+// debugger: console.log(numOne, numTwo, operation, res);
--- scriptnotes.js
+++ scriptnotes.js
@@ -41,13 +41,3 @@ numButtons.forEach((button) => {
     })
 })
 
-
-// pressing first number will be numOne, then pressing an operation, then pressing second number will be numTwo
-// take numOne, see what the operation is, then operate numTwo onto numOne
-
-// add event listener to each number
-
-// if numOne doesnt exist, make the number numOne, if it does, make it numTwo
-
-// after the operation and numbers are pressed, user will press either equals or another operation,
-// so for equals, need to just evaluate function
