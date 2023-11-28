4d37614b8a1dfa71e4baab7a41e37b2a953cefd0
Muhammad Alvi <149120789+MuhammadAlvi03@users.noreply.github.com>
2023-11-27 19:09:34 -0600

    add actions to buttons and tweak ui

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
-const zeroButton = document.querySelector('.zero');
-const oneButton = document.querySelector('.one');
-const twoButton = document.querySelector('.two');
-const threeButton = document.querySelector('.three');
-const fourButton = document.querySelector('.four');
-const fiveButton = document.querySelector('.five');
-const sixButton = document.querySelector('.six');
-const sevenButton = document.querySelector('.seven');
-const eightButton = document.querySelector('.eight');
-const nineButton = document.querySelector('.nine');
+const zeroButton = document.querySelector('#zero');
+const oneButton = document.querySelector('#one');
+const twoButton = document.querySelector('#two');
+const threeButton = document.querySelector('#three');
+const fourButton = document.querySelector('#four');
+const fiveButton = document.querySelector('#five');
+const sixButton = document.querySelector('#six');
+const sevenButton = document.querySelector('#seven');
+const eightButton = document.querySelector('#eight');
+const nineButton = document.querySelector('#nine');
+
+const numButtons = document.querySelectorAll('.numButton');
 
 const addButton = document.querySelector('.add');
 const subtractButton = document.querySelector('.subtract');
@@ -16,10 +18,19 @@ const divideButton = document.querySelector('.divide');
 
 const equalsButton = document.querySelector('.equals');
 
+const opButtons = document.querySelectorAll('.opButton');
+
 const clearButton = document.querySelector('.clear');
 const negativeButton = document.querySelector('.negative');
 const decimalButton = document.querySelector('.decimal');
 
+const display = document.querySelector('.display');
+
+let numOne;
+let numTwo;
+let operation;
+let res;
+let addproduct;
 
 
 function myAdd(numOne, numTwo) {
@@ -27,36 +38,63 @@ function myAdd(numOne, numTwo) {
 }
 
 function mySubtract(numOne, numTwo) {
-    return numOne - numTwo;
+    res = numOne - numTwo;
 }
 
 function myMultiply(numOne, numTwo) {
-    return numOne * numTwo;
+    res = numOne * numTwo;
 }
 
 function myDivide(numOne, numTwo) {
-    return numOne / numTwo;
+    res = numOne / numTwo;
 }
 
-let numOne;
-let numTwo;
-let operation;
 
 
-function operate(numOne, numTwo, operation) {
-    let res;
-    if (operation === 'add') {
-        res = myAdd(numOne, numTwo)
+function operate(operation) {
+    if (operation === '+') {
+        res = myAdd();
+        console.log(alert('hi')); // 
     }
-    if (operation === 'subtract') {
+    if (operation === '-') {
         res = mySubtract(numOne, numTwo)
     }
-    if (operation === 'multiply') {
+    if (operation === '×') {
         res = myMultiply(numOne, numTwo)
     }
-    if (operation === 'divide') {
+    if (operation === '÷') {
         res = myDivide(numOne, numTwo)
     }
-    return res
+    console.log(res);
 }
 
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (display.textContent.length < 12) {
+        display.textContent += button.textContent;
+        }
+    })
+})
+
+opButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        if (!numOne) {
+            numOne = Number(display.textContent);
+        } else if (numOne) {
+            numTwo = Number(display.textContent);
+        }
+        operation = button.textContent;
+        console.log(operation); //
+    })
+})
+
+
+clearButton.addEventListener('click', () => {
+    display.textContent = '';
+})
+
+equalsButton.addEventListener('click', () => {
+    numTwo = Number(display.textContent);
+    console.log(equalsButton.textContent);
+    operate();
+});
--- scriptnotes.js
+++ scriptnotes.js
@@ -29,3 +29,25 @@ function myDivide(initial, ...nums) {
     }
     return res;
 }
+
+numButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        console.log(button.textContent);    //
+        if (!numOne) {
+            numOne = Number(button.textContent);
+        } else if (numOne) {
+            numTwo = Number(button.textContent);
+        }
+    })
+})
+
+
+// pressing first number will be numOne, then pressing an operation, then pressing second number will be numTwo
+// take numOne, see what the operation is, then operate numTwo onto numOne
+
+// add event listener to each number
+
+// if numOne doesnt exist, make the number numOne, if it does, make it numTwo
+
+// after the operation and numbers are pressed, user will press either equals or another operation,
+// so for equals, need to just evaluate function
